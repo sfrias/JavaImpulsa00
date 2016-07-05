8f74dfc2093c4a8f030d019ac4c4c80d07c926fc
sfrias <sfrias1@gmail.com>
2016-07-05 04:26:16 +0200

    First Commit for Android-Java Basics

---
+++ src/net/visualia/tgnimpulsa/Main.java
@@ -0,0 +1,17 @@
+package net.visualia.tgnimpulsa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DoG doggy = new DoG();
+        doggy.setName("Rayo");
+        doggy.setOwnerName("Nubarrón");
+        doggy.setSkinColor("Blanco");
+
+        System.out.println("Birth:"+doggy.getBirthDate());
+        System.out.println("Name:"+doggy.getName());
+        System.out.println("Owner:"+doggy.getOwnerName());
+        System.out.println("ColorPelo:"+doggy.getSkinColor());
+        System.out.println(
+    }
+}
